e46418ff0cef28fa5b7013eca379db36c4344b4f
storezhang <storezhang@gmail.com>
2023-05-17 09:49:07 +0800

    feat(网关): 增加原始数据绑定到二进制数组

--- raw.go
+++ raw.go
@@ -8,7 +8,7 @@ type raw struct {
 	// 前缀
 	Prefix string `json:"prefix" yaml:"prefix" xml:"prefix" toml:"prefix"`
 	// 后缀
-	Suffix string `yaml:"suffix" yaml:"suffix" xml:"suffix" toml:"suffix"`
+	Suffix string `json:"suffix" yaml:"suffix" xml:"suffix" toml:"suffix"`
 	// 包含
 	Contains string `default:"raw" json:"contains" yaml:"contains" xml:"contains" toml:"contains"`
 }
