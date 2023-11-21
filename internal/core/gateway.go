30da0712fd238adbb0a25c9c5095670b9b54bc04
storezhang <storezhang@gmail.com>
2023-11-21 19:54:18 +0800

    feat(网关): 增加状态错误

--- internal/core/gateway.go
+++ internal/core/gateway.go
@@ -34,29 +34,29 @@ func (g *Gateway) Status(ctx context.Context, code int) {
 	}
 }
 
-func (g *Gateway) Error(code codes.Code, err error) error {
-	return status.Error(code, err.Error())
-}
-
-func (g *Gateway) Exception(code codes.Code, exception exc.Exception) error {
-	return status.Error(code, exception.Error())
-}
-
 func (g *Gateway) ValidationError(err error) error {
-	return g.Error(http.StatusBadRequest, err)
+	return g.error(http.StatusBadRequest, err)
 }
 
 func (g *Gateway) ServerError(err error) error {
-	return g.Error(http.StatusInternalServerError, err)
+	return g.error(http.StatusInternalServerError, err)
 }
 
 func (g *Gateway) ServerException(code int, fields gox.Fields[any]) (err error) {
 	exception := exc.NewException(code, "服务器错误，客户端需要根据返回中的`code`码来确认具体是什么错误", fields...)
-	err = g.Exception(http.StatusInternalServerError, exception)
+	err = g.exception(http.StatusInternalServerError, exception)
 
 	return
 }
 
 func (g *Gateway) Notfound() error {
-	return g.Error(http.StatusNotFound, exc.NewMessage("未找到资源"))
+	return g.error(http.StatusNotFound, exc.NewMessage("未找到资源"))
+}
+
+func (g *Gateway) error(code codes.Code, err error) error {
+	return status.Error(code, err.Error())
+}
+
+func (g *Gateway) exception(code codes.Code, exception exc.Exception) error {
+	return status.Error(code, exception.Error())
 }
